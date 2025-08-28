e2ee62190d65d393e12391bc8c837fd1842143a3
ccaiocesarcardoso <ccaiocesar648@gmail.com>
2025-08-28 16:16:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'tottenham-fc-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/assets/icon-192x192.png',
-  '/assets/icon-512x512.png',
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'assets/icon-192x192.png',
+  'assets/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
